55c8dbcfb529e7e8f769277976a779b332ce6bfd
Aleksei Petryuk <mybiysk@yandex.ru>
2022-03-30 23:24:31 +0700

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,30 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println("Первый привет миру: " + bean.getMessage());
+
+        HelloWorld beanSecond =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Второй привет миру: " + beanSecond.getMessage());
+
+        Cat catFirst =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("Первый кот сказал: " + catFirst.getCatMessage());
+        Cat catSecond =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("Второй кот сказал: " + catSecond.getCatMessage());
+
+        System.out.println("Равны ли объекты без @Scope: " + bean.equals(beanSecond));
+        System.out.println("Равны ли объекты со @Scope: " + catFirst.equals(catSecond));
+
+
     }
 }
